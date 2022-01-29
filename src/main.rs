22edc6ca04c40655a4d28d6027dc9addd4797adb
chris <chris@pop-os.localdomain>
2022-01-29 16:55:47 -0700

    feature: event handler

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,75 @@
 #[macro_use]
 extern crate clap;
 extern crate serde;
+
+use std::fs;
+use std::sync::mpsc::channel;
+use std::time::Duration;
 use clap::App;
+use notify::{DebouncedEvent, RecursiveMode, Watcher, watcher};
+use notify::DebouncedEvent::{Create, Remove, Write};
 use reqwest::{Error, Response};
 use serde::{Serialize};
+use futures::executor::block_on;
 
-fn main()  -> Result<(), &'static str> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let yaml = load_yaml!("cli.yaml");
     let arg_matches = App::from_yaml(yaml).get_matches();
     let token = arg_matches.value_of("token").unwrap();
-    let config = GameConfig {
+    let config = Config {
         bearer_token: token.to_string(),
         port: String::from("9990"),
-        url: String::from("localhost") };
+        url: String::from("localhost"),
+        valid_extensions: vec![".script".to_string(), ".js".to_string(), ".ns".to_string(), ".txt".to_string()],
+    };
+    let (sender, receiver) = channel();
+    let mut watcher = watcher(sender, Duration::from_secs(2)).unwrap();
+    watcher.watch("/home/chris/rust", RecursiveMode::Recursive).unwrap();
+    loop {
+        match receiver.recv() {
+            Ok(event) => handle_event(&config, &event).unwrap(),
+            Err(e) => println!("error: {:?}", e),
+        }
+    }
+}
+
+fn handle_event(config: &Config, event: &DebouncedEvent) -> Result<(), Box<dyn std::error::Error>> {
+    match event {
+        Write(p) | Create(p) => {
+            if config.valid_extensions.contains(&p.extension().unwrap().to_str().unwrap().to_owned()) {
+                // file contents must be encoded to base64
+                let code = base64::encode(fs::read_to_string(p.as_path()).unwrap());
+                let request = BitburnerRequest {
+                    filename: String::from(p.file_name().unwrap().to_str().unwrap()),
+                    code
+                };
+                block_on(send_file(config, request))?;               
+            }
+        },
+        Remove(path) => println!("filename: {:?}",path.file_name().unwrap()),
+        _ => ()
+    }
     Ok(())
 }
 
-async fn send_file(config: GameConfig, bitburner_request: BitburnerRequest) -> Result<Response, Error> {
+async fn send_file(config: &Config, bitburner_request: BitburnerRequest) -> Result<Response, Error> {
     let client = reqwest::Client::new();
     let body = serde_json::to_string(&bitburner_request).unwrap();
     let url = format!("{}:{}", config.url, config.port);
+    let token = config.bearer_token.clone();
     return client.put(url)
-        .bearer_auth(config.bearer_token)
+        .bearer_auth(token)
         .body(body)
         .send()
         .await
 }
 
 #[derive(Debug)]
-struct GameConfig {
+struct Config {
     bearer_token: String,
     port: String,
     url: String,
+    valid_extensions: Vec<String>,
 }
 
 #[derive(Debug, Serialize)]
