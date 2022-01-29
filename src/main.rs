715cf97be2bc57341055112b4b1edefef0b41449
chris <chris@pop-os.localdomain>
2022-01-29 14:49:30 -0700

    refactor: use struct in signature

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #[macro_use]
 extern crate clap;
+extern crate serde;
 use clap::App;
 use reqwest::{Error, Response};
+use serde::{Serialize};
 
 fn main()  -> Result<(), &'static str> {
     let yaml = load_yaml!("cli.yaml");
@@ -14,12 +16,8 @@ fn main()  -> Result<(), &'static str> {
     Ok(())
 }
 
-async fn get_response(config: GameConfig, filename: &str, code: &str) -> Result<Response, Error> {
+async fn send_file(config: GameConfig, bitburner_request: BitburnerRequest) -> Result<Response, Error> {
     let client = reqwest::Client::new();
-    let bitburner_request = BitburnerRequest {
-        filename: filename.to_string(),
-        code: code.to_string()
-    };
     let body = serde_json::to_string(&bitburner_request).unwrap();
     let url = format!("{}:{}", config.url, config.port);
     return client.put(url)
