1761d1e26ba88fc67264a6347e859ce589d3ed28
chris <chris@pop-os.localdomain>
2022-01-29 21:28:27 -0700

    log: env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 #[macro_use]
 extern crate clap;
+#[macro_use]
+extern crate log;
 extern crate serde;
 
+use env_logger::Env;
 use std::fs;
 use std::sync::mpsc::channel;
 use std::time::Duration;
@@ -14,15 +17,20 @@ use futures::executor::block_on;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let env = Env::default()
+        .filter_or("LOG_LEVEL", "info")
+        .write_style_or("LOG_STYLE", "always");
+    env_logger::init_from_env(env);
     let config = get_config()?;
-    println!("bitburner-oxide initialized with config:\n{:?}", &config);
+    info!("bitburner-oxide initialized with config:");
+    info!("{:?}", &config);
     let (sender, receiver) = channel();
     let mut watcher = watcher(sender, Duration::from_secs(2)).unwrap();
     watcher.watch(&config.directory, RecursiveMode::Recursive).unwrap();
     loop {
         match receiver.recv() {
             Ok(event) => handle_event(&config, &event).unwrap(),
-            Err(e) => println!("error: {:?}", e),
+            Err(e) => error!("error: {:?}", e),
         }
     }
 }
@@ -44,16 +52,15 @@ fn get_config() -> Result<Config, Box<dyn std::error::Error>> {
     })
 }
 
-
 fn handle_event(config: &Config, event: &DebouncedEvent) -> Result<(), Box<dyn std::error::Error>> {
-    println!("event: {:?}", event);
+    debug!("event: {:?}", event);
     match event {
         Write(p) | Create(p) | Chmod(p) => {
             if p.extension().is_some() && config.valid_extensions.contains(&p.extension().unwrap().to_str().unwrap().to_owned()) {
                 // file contents must be encoded to base64
                 let code = base64::encode(fs::read_to_string(p.as_path()).unwrap());
                 let filename = String::from(p.file_name().unwrap().to_str().unwrap());
-                println!("file change detected for file: {:?}", &filename);
+                info!("file change detected for file: {:?}", &filename);
                 let request = BitburnerRequest {
                     filename,
                     code
@@ -61,7 +68,7 @@ fn handle_event(config: &Config, event: &DebouncedEvent) -> Result<(), Box<dyn s
                 block_on(send_file(config, request))?;               
             }
         },
-        Remove(path) => println!("file deleted: {:?}",path.file_name().unwrap()),
+        Remove(path) => trace!("file deleted: {:?}",path.file_name().unwrap()),
         _ => ()
     }
     Ok(())
