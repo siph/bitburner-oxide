497a4aa423589a9ce416a95196104873013b034e
siph <siph@github>
2022-05-25 07:35:41 -0600

    doc: update to reflect new features

--- src/app_args.rs
+++ src/app_args.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 /// Bitburner-oxide will automatically push modified or created script files to a running Bitburner game server.   
 /// 
 /// If ran from the same directory as the scripts the --directory flag is not needed.   
-/// All managed files must exist in the top level directory; bitburner-oxide does not manage nested folders.   
+/// Bitburner can manage all child files and directories of the chosen parent directory.
 /// 
 /// Authentication is done by passing in the bearer-token via --token.   
 /// Alternatively, the bearer-token can be placed in a file named 'token' in the chosen directory.   
